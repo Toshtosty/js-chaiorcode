c13a6f2f1acf85089c72ec0c9bd0cc57ec848e88
Toshtosty <159453894+Toshtosty@users.noreply.github.com>
2024-06-13 14:22:21 +0000

    conversion of datatype

---
+++ 01_basics/03_DataTypeConversion.js
@@ -0,0 +1,20 @@
+let score="33abc"
+// score- null, undefined , bool(true/false) , string
+// converting values to Number:  "33"=>33   ,   "33abc"=>NaN , true=>1 , null=>0, undefined=NaN
+
+
+// console.log(typeof score)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+// // NaN is not in number i.e. string is converted into number but value cant be converted into number
+
+let sample="toshita"
+let boolnew=Boolean(sample)
+console.log(boolnew)
+// 1=>true , ""=>false ,"toshita"=>true
+
+let x=33
+let y=String(x)
+console.log("y=",y)
+console.log(typeof y)
