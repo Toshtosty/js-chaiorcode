664dca780f356d7360e3a2c191318a213bf6b640
Toshtosty <159453894+Toshtosty@users.noreply.github.com>
2024-06-13 09:14:20 +0000

    i am leaarning newww

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId =144553
+let accountEmail = "toshita1@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+// accountId = 2
+
+
+
+accountEmail="tototo@gmail.com"
+accountPassword="433243"
+accountCity="dehradun"
+console.log(accountId)
+console.table([accountId,accountPassword,accountEmail,accountCity])
