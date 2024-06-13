d4e6d1e5ae72f043d1b6a893581ca77bd6c9091c
Toshtosty <159453894+Toshtosty@users.noreply.github.com>
2024-06-13 16:50:26 +0000

    5

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,19 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2==1);
+// // console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+console.log(null>0)
+console.log(null>=0);
+console.log(null==0);
+
+
+
+
+// strict check=> === this checks data type as well 
+// in normal check => == datatype conversions take place   
