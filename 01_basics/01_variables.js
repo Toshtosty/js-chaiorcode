de7703f4ad7f2622fb2b974aeed2f4374f242316
Toshtosty <159453894+Toshtosty@users.noreply.github.com>
2024-06-13 09:26:25 +0000

    data-type

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -5,7 +5,14 @@ accountCity = "jaipur"
 
 // accountId = 2
 
+/*
 
+prefer not to use var
+ because of issue in block scope and functional scope
+
+
+
+*/
 
 accountEmail="tototo@gmail.com"
 accountPassword="433243"
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hellloooo ")  ======> we are using nodejs not browser
+console.log(3-2)
+console.log("tototototo")
+
+
+//number
+//bigint
+//string=>""
+//boolean=> true/false
+//null => standalone value(empty value)
+//undefined => declared but not given any value
+// symbol => unique
+
+// object
+
+console.log(typeof null)
