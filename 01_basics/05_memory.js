d500352e29b8898c7acc28cea9007e22951d7e6f
Toshtosty <159453894+Toshtosty@users.noreply.github.com>
2024-06-16 07:59:43 +0000

    memory

---
+++ 01_basics/05_memory.js
@@ -0,0 +1,24 @@
+// stack memory used in primitive type
+//heap =>non primitive
+
+let yname="lalalajjdjd"
+let aname=yname
+ aname="ejfjv"
+
+ console.log(aname)
+ console.log(yname)
+
+ let u1={
+    email:"todhita@gmail.com",
+    names:"ferg",
+    age:43
+ }
+
+ let u2=u1
+
+ console.log(u1.email)
+ console.log(u2.email);
+ u2.email="hemu@gmail.com"
+
+ console.log(u1.email)
+ console.log(u2.email);
